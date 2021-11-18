d6635ef81540138223d891dbc802ea24e5f507d9
naivenave <91294407+naivenave@users.noreply.github.com>
2021-11-18 15:43:18 +0530

    Update polyfills.js

--- polyfills.js
+++ polyfills.js
@@ -59,3 +59,33 @@ console.log(newFilter([1,2,3,4],checkEven)) // In console logs out [ 2, 4 ]
 console.log(newFilter([],checkEven)) // In console logs out Please pass non-empty array
 console.log(newFilter("[1,2,3,4]",checkEven)) // In console logs out "Please pass an array"
 
+// Polyfill for Reduce function
+
+const newReduce=(cbfunction, array, initial_value)=>{
+  try{
+    if(!Array.isArray(array)){
+      throw "Please pass an array"
+    }
+    if(array.length<1){
+      throw "Please pass non-empty array"
+    }
+  let final_answer = 0;
+    if(initial_value){
+      final_answer =initial_value    
+    }
+  for(let i=0;i<array.length;i++){
+   final_answer = cbfunction(array[i], final_answer)
+  }
+  return final_answer;
+  }catch(err){
+    return (err)
+  }
+}
+
+function reducer(x, y){
+  return x+y;
+}
+
+console.log(newReduce(reducer, [1]))   // Logs out 1
+console.log(newReduce(reducer, [1], 5))   // Logs out 6
+console.log(newReduce(reducer, []))   // Logs out Please pass non-empty array
